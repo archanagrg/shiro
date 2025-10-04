f02aeb4ded525cc7a832c0093f8b46d282b35b5c
Archan Gurung <archanagurung2k21@gmail.com>
2025-10-04 08:26:36 +0545

    first-commit

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ import orderRouter from './routes/orderRoute.js'
 
 // App Configuration
 const app = express()
-const port= process.env.PORT || 4000  
+const port= process.env.PORT || 4000;
 connectDB()
 connectCloudinary()
 
